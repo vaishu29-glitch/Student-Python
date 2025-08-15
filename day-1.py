2b80a74f306473077426dfa16de71264d43e05ec
vaishu29-glitch <vaishnavikalikivayi@gmail.com>
2025-08-15 15:53:18 +0530

    Update day-1.py
    In day-1 practice session i worked on  basic Strings & Pattern concepts .the task cover basic string manipulation, pattern checking, and character frequency analysis .

--- day-1.py
+++ day-1.py
@@ -1,22 +1,22 @@
 # To print given string is palindrome or not
 
-"""s=input("enter string:")
+s=input("enter string:")
 if(s[::-1]==s):
     print("palindrome")
 else:
-    print("not palindrome")"""
+    print("not palindrome")
 
 #input:  malayalam
 #output: palindrome
 
 #counting the frequency of each character in a string
 
-"""s=input("enter string:")
+s=input("enter string:")
 res=''
 for i in s:
     if i not in res:
         print(i,'-',s.count(i))
-        res+= i"""
+        res+= i
 
 #input: enter string:college
 #output: c - 1
@@ -36,16 +36,16 @@ while i>=0:
 
 #remove all spaces from a given string
 
-"""s = input("Enter string: ")
+s = input("Enter string: ")
 s = s.replace(" ", "")
-print(s)"""
+print(s)
 
 #input:  Enter string: pace college
 #output: pacecollege
 
 #no.of vowels and consonants in a string
 
-"""s=input("enter string: ")
+s=input("enter string: ")
 vowels ='aeiou'
 v_count=0
 c_count=0
@@ -56,10 +56,11 @@ for ch in s:
         else:
             c_count += 1
 print("vowels:",v_count)
-print("consonants:",c_count)"""
+print("consonants:",c_count)
 
 #input:  enter string: pace college
 #output: vowels: 5
 #        consonants: 6
             
         
+
